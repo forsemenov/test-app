00d87c9c87f86cd27c8ef149be6eea10a0080d6a
AlexeiSemenov <forsemenov@rambler.ru>
2016-01-21 01:59:29 +0200

    Refactoring

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
   before_action :authenticate_user!, except: [:show, :index]
+  before_action :find_post , except: [:create, :index]
 
   def index
     @posts = Post.all
@@ -7,7 +8,6 @@ class PostsController < ApplicationController
   end
 
   def show
-    @post = Post.find(params[:id])
     @comments = Comment.where(post_id: @post.id)
     @comment = Comment.new
   end
@@ -18,23 +18,26 @@ class PostsController < ApplicationController
   end
 
   def edit
-    @post = Post.find(params[:id])
   end
 
   def update
-    @post = Post.find(params[:id])
     @post.update(post_params)
     redirect_to posts_path
   end
 
   def destroy
-    @post = Post.find(params[:id]).destroy
+    @post.destroy
     redirect_to posts_path
   end
 
   private
 
+  def find_post
+    @post = Post.find(params[:id])
+  end
+
   def post_params
     params.require(:post).permit(:title, :body)
   end
+
 end
